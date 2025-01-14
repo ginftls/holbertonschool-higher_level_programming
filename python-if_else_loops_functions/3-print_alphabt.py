20ca93aee2f9661517017962e33d6c20f23ba525
ginftls <scdr1905@gmail.com>
2025-01-14 06:42:52 -0500

    no line alphab+ hexa

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join([chr(i) for i in range(97, 123) if chr(i) not in 'qe']), end="")
+for ascii_value in range(97, 123):
+    if not (ascii_value == 101 or ascii_value == 113):
+        print("{}".format(chr(ascii_value)), end="")
