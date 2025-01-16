103b941fae2bcc6c4f53cea29817e7ac9b2cd00a
ginftls <scdr1905@gmail.com>
2025-01-16 18:38:34 -0500

    0-add.py2

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-# Import the add function from add_0.py
 from add_0 import add
 
-# Define the variables a and b
 a = 1
 b = 2
-
-# Print the result using string formatting
 print("{} + {} = {}".format(a, b, add(a, b)))
