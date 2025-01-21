21c90f786f0a78a76018760a9f01c9145f413b07
ginftls <scdr1905@gmail.com>
2025-01-21 10:09:48 -0500

    11 initial

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
         return my_list
-    # Create a new list without the element at the specified index
-    new_list = my_list[:idx] + my_list[idx+1:]
-    # Return the new list
-    return new_list
+    # Delete the item at the specified index in place
+    del my_list[idx]
+    # Return the modified list
+    return my_list
