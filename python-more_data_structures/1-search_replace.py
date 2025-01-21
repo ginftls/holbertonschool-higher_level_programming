476f28fd41a4cd2ea7d1f67cbb2096c3811d4df5
ginftls <scdr1905@gmail.com>
2025-01-21 10:53:47 -0500

    01 initial

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,4 @@
+def search_replace(my_list, search, replace):
+    """Replaces all occurrences of search with replace in a new list."""
+    # Use a list comprehension to create a new list with the replaced elements
+    return [replace if element == search else element for element in my_list]
