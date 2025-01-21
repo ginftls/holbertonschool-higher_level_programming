2f43061e35ada71745df144daaae3edbfe6be12e
ginftls <scdr1905@gmail.com>
2025-01-21 11:52:46 -0500

    05 initial

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary."""
+    # Use the len() function to count the keys in the dictionary
+    return len(a_dictionary)
