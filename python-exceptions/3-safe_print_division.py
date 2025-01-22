1850e3d5b6682417136fc9d974a37b59f2a330a6
ginftls <scdr1905@gmail.com>
2025-01-22 09:44:03 -0500

    03 new code

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
