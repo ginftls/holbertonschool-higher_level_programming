b5af7da7ccfcd4366fc13c965bd914f7b67968da
ginftls <scdr1905@gmail.com>
2025-01-30 10:09:16 -0500

    feat: enhance Square class with size validation in constructor

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" Class Square that defines a square """
+""" This module creates a Square Class"""
 
 
 class Square:
-    """ Class Square """
+    """ Square class with size validation """
     __size = None
 
-    def __init__(self, size=__size):
-        if size is not None:
-            self.__size = size
-            
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
