ed792021982297867e697c02865b3b8c508b73a6
ginftls <scdr1905@gmail.com>
2025-01-30 08:38:11 -0500

    feat: implement Square class with area method and main script for testing

---
+++ python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with a private instance attribute size.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the current square area.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+    
