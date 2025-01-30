07fc02dd386983f9ddca176c429766cad83ccd54
ginftls <scdr1905@gmail.com>
2025-01-30 10:14:04 -0500

    refactor: improve Square class docstring and constructor documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-Module defines empty Square class that initializes square of given size.
-"""
-
-
 class Square:
     """
-    Square class. This class is used to create a square of a given size.
+    A class that defines a square with a private size attribute.
+
+    Attributes:
+        __size (int): The size of the square.
     """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
