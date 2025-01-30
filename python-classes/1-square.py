a555ad6ec86b310a8ab45b69cd518368ad3a1bc3
ginftls <scdr1905@gmail.com>
2025-01-30 10:18:17 -0500

    refactor: simplify Square class definition and initialization

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
+""" Class Square that defines a square """
+
+
 class Square:
-    """
-    A class that defines a square with a private size attribute.
+    """ Class Square """
+    __size = None
 
-    Attributes:
-        __size (int): The size of the square.
-    """
-
-    def __init__(self, size):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
+    def __init__(self, size=__size):
+        if size is not None:
+            self.__size = size
