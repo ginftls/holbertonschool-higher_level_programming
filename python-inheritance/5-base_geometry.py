e4b2c93446c9d5cae1ea8d992fc7a4cd289fe506
ginftls <scdr1905@gmail.com>
2025-02-05 06:38:20 -0500

    feat: add BaseGeometry class as a base for geometric operations

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometric operations.
+    Currently, it does not implement any methods or attributes.
+    """
+    pass
