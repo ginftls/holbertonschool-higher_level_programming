7b87520df7f2f14537e12c2f02c8ebc31311d54f
ginftls <scdr1905@gmail.com>
2025-02-05 06:27:11 -0500

    refactor: simplify inherits_from function and update docstring

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
-"""
-Module that provides a function to check if an object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class.
-"""
+""" Module for the inherits_from method """
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if obj is an instance of a class that inherited
-    (directly or indirectly) from a_class. Otherwise, returns False.
 
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-
-    Returns:
-        bool: True if obj inherits from a_class, False otherwise.
-    """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
