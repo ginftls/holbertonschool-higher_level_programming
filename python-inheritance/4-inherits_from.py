0fbe4cfcb46b6a0cc12cd1d3802e79e25e1937ba
ginftls <scdr1905@gmail.com>
2025-02-05 06:32:45 -0500

    docs: update docstring for inherits_from function to clarify its purpose and behavior

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-
+    """ Method that returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
