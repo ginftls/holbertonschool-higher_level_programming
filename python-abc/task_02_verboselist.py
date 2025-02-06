b8109ac63aac4b0c84e4ef9bf127e4dee0e6dd41
ginftls <scdr1905@gmail.com>
2025-02-06 13:21:42 -0500

    feat: implement VerboseList class with enhanced list operations and logging

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,28 @@
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, iterable):
+        length = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{length}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        super().pop(index)
+        print(f"Popped [{item}] from the list.")
+
+
+if __name__ == "__main__":
+    vl = VerboseList([1, 2, 3])
+    vl.append(4)
+    vl.extend([5, 6])
+    vl.remove(2)
+    vl.pop()
+    vl.pop(0)
