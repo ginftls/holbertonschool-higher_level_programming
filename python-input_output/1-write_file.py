892375ce82e07a1bc8c9128bda4c2e7b3b287837
ginftls <scdr1905@gmail.com>
2025-02-11 07:37:56 -0500

    refactor: update docstring and formatting in write_file function for clarity

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+"""
+Module 1-write_file
+This module contains a function that writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns
-    the number of characters written.
+    Writes a string to a text file (UTF8) and returns the
+    number of characters written.
 
     Args:
         filename (str): The name of the file to write to.
         text (str): The string to write to the file.
 
     Returns:
-        int: The number of characters written to the file.
+        int: The number of characters written.
     """
-    with open(filename, 'w', encoding='utf-8') as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
