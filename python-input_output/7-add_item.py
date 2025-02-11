f8ed4885a7675a1cca5b4e91ff731630048daa36
ginftls <scdr1905@gmail.com>
2025-02-11 11:20:38 -0500

    fix: update import statements for save_to_json_file and load_from_json_file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ Script that adds all arguments to a Python list and saves them to a JSON file.
 
 import sys
 import os
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 FILENAME = "add_item.json"
 
