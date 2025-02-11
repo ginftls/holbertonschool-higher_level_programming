0b8419193c2e17dd20d7109f3bbbdd3f41fa8283
ginftls <scdr1905@gmail.com>
2025-02-11 07:10:07 -0500

    refactor: update read_file function docstring and improve variable naming for clarity

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+""" Module for read_file method """
+
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints its content to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
+    """ function that reads a text file (UTF8) and prints it to stdout """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end='')
