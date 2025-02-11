1e397b87ec4ff7b0fe9d565dc2f1aaf96a9818fd
ginftls <scdr1905@gmail.com>
2025-02-11 10:47:57 -0500

    refactor: improve module docstring and parameter description in to_json_string function

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+
+
+"""
+Module 3-to_json_string
+This module contains a function that returns
+the JSON representation of an object (string).
+"""
 import json
 
 
@@ -7,7 +14,7 @@ def to_json_string(my_obj):
     Returns the JSON representation of an object (string).
 
     Args:
-        my_obj: The object to serialize into JSON.
+        my_obj: The object to serialize.
 
     Returns:
         str: The JSON representation of the object.
