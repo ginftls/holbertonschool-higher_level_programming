cb5ffbbb173ec932957a00e222f4dfc8dd98a77c
ginftls <scdr1905@gmail.com>
2025-02-18 07:27:55 -0500

    feat: add /info endpoint to SimpleAPIHandler for API version and description

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,6 +25,15 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.end_headers()
             status = {"status": "OK"}
             self.wfile.write(json.dumps(status).encode("utf-8"))
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode("utf-8"))
         else:
             self.send_response(404)
             self.send_header("Content-type", "application/json")
