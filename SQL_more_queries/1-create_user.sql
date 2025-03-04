6dc63c65e629f24cdba716ef114c6bc3bd781e5e
ginftls <scdr1905@gmail.com>
2025-03-04 11:23:23 -0500

    createuser

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates user_0d_1 with all privileges and sets password.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
