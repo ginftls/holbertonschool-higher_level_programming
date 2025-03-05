56e3bee284648aee6cd7b868a5a42e2e06bd0e38
ginftls <scdr1905@gmail.com>
2025-03-05 09:29:13 -0500

    countshows05

--- SQL_more_queries/ 13-count_shows_by_genre.sql
+++ SQL_more_queries/ 13-count_shows_by_genre.sql
@@ -1,11 +1,2 @@
--- Select 'name' column from 'tv_genres' table, & count 'show_id' column from 'tv_show_genres' table
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-    -- From 'tv_genres' table
-FROM tv_genres
-    -- Inner join 'tv_show_genres' table on 'id' in 'tv_genres' table & 'genre_id' in 'tv_show_genres' table
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    -- Group results by 'name' in 'tv_genres' table
-GROUP BY
-    tv_genres.name
-    -- Order results by count of 'show_id' in descending order
-ORDER BY COUNT(tv_show_genres.show_id) DESC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
