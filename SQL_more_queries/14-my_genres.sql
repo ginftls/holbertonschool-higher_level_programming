825362ab4358748dfbff37ec5215d70d6dede00e
ginftls <scdr1905@gmail.com>
2025-03-05 08:11:20 -0500

    selectdistinctadd

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
--- Lists genres of the show Dexter
-SELECT genres.name
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN genres ON tv_show_genres.genre_id = genres.id
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+SELECT DISTINCT name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY name ASC;
