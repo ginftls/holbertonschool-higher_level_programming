7db4d1c6da4362567a6ec80d5ec32979913573d4
ginftls <scdr1905@gmail.com>
2025-03-05 08:07:25 -0500

    mygenres

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Lists genres of the show Dexter
+SELECT genres.name
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN genres ON tv_show_genres.genre_id = genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY genres.name ASC;
