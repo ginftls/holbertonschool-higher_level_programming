b288b9c769496805a8c435f2baa6283c9c4b28ec
ginftls <scdr1905@gmail.com>
2025-03-06 07:12:21 -0500

    selectstates

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Module to list all states from a MySQL database."""
+
 import MySQLdb
 import sys
 
@@ -7,7 +10,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Connect to MySQL server
+    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -19,14 +22,13 @@ if __name__ == "__main__":
     # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query
+    # Execute the SQL query to select all states ordered by id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     # Fetch all rows and print them
     for row in cursor.fetchall():
         print(row)
 
-    # Close the cursor and connection
+    # Close cursor and database connection
     cursor.close()
     db.close()
-    
