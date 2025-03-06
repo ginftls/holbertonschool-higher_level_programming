5b3a807c445a0e9c18f149edc45107d9e3171379
ginftls <scdr1905@gmail.com>
2025-03-06 07:22:09 -0500

    02

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module to list all states from a MySQL database."""
-
+"""Module that lists all states from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     # Get command line arguments
@@ -22,13 +22,13 @@ if __name__ == "__main__":
     # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to select all states ordered by id
+    # Execute the SQL query
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
     # Fetch all rows and print them
     for row in cursor.fetchall():
         print(row)
 
-    # Close cursor and database connection
+    # Close the cursor and database connection
     cursor.close()
     db.close()
