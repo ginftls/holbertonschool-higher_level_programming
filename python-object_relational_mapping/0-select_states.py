eedac0f03afd3de8529e68f239e8d1d15ad51665
ginftls <scdr1905@gmail.com>
2025-03-07 05:29:47 -0500

    selectstates

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""Module that query USA States from a database table"""
+
+""" Lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    """ Lists all states from the database hbtn_0e_0_usa
+
+        Usage: ./0-select_states.py <mysql username>
+        <mysql password> <database name>
+
+        Arguments:
+            mysql username: username to connect the mySQL database
+            mysql password: password to connect the mySQL database
+            database name: name of the database to connect
+        """
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to select all states ordered by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the executed query
+    rows = cursor.fetchall()
+
+    # Print each row
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,4 @@
+python-object_relational_mapping
+================================
+
+This is a simple example of how to use Python to query a relational database.
